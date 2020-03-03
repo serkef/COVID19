45ae6b3f5845308bbc3af6b64f5afdb577c1aca4
Sergio Kef <sergiokef@gmail.com>
2020-03-03 21:54:20 +0100

    Switch to service account, transform data

--- twitter/utilities.py
+++ twitter/utilities.py
@@ -1,14 +1,12 @@
 """ custom utilities """
-
+import json
 import logging
-import os
+import sqlite3
 from logging.handlers import TimedRotatingFileHandler
-import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 
+import requests
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
 import tweepy
 
 from .config import (
@@ -18,11 +16,45 @@ from .config import (
     TWITTER_ACCESS_TOKEN,
     TWITTER_ACCESS_TOKEN_SECRET,
     ENV,
+    DB_PATH,
+    DB_CREATE_RAW_TABLE,
+    DB_CREATE_POSTS,
+    DB_CREATE_LATEST_DAILY_TABLE,
+    SLACK_WEBHOOK_URL,
+    STATUS_TEMPLATE,
+    GSHEET_API_SERVICE_ACCOUNT_FILE,
+    GSHEET_API_SCOPES,
 )
 
 
+def create_tables():
+    conn = sqlite3.connect(f"{DB_PATH}")
+    cursor = conn.cursor()
+    cursor.execute(DB_CREATE_RAW_TABLE)
+    cursor.execute(DB_CREATE_LATEST_DAILY_TABLE)
+    cursor.execute(DB_CREATE_POSTS)
+    conn.commit()
+    conn.close()
+
+
+def slack_status(status):
+    logger = logging.getLogger(f"{__name__}.slack_status")
+    logger.info(f"Posting status {status!r}")
+
+    requests.post(
+        SLACK_WEBHOOK_URL,
+        data=json.dumps({"text": status}),
+        headers={"Content-Type": "application/json"},
+    )
+
+
+def create_status(day, territory, value):
+    return STATUS_TEMPLATE.format(day=day, territory=territory, value=value)
+
+
 def tweet_status(status):
-    logging.info(f"Posting status {status!r}")
+    logger = logging.getLogger(f"{__name__}.tweet_status")
+    logger.info(f"Posting status {status!r}")
 
     if ENV == "production":
         auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_KEY_SECRET)
@@ -30,34 +62,17 @@ def tweet_status(status):
         api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
         api.update_status(status)
     else:
-        logging.info("Skipping - not in production")
+        logger.info("Skipping - not in production")
 
 
 def get_gsheet_api():
     """ Initializes Google API. Taken from quickstart example """
 
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists("token.pickle"):
-        with open("token.pickle", "rb") as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                "/home/serkef/Downloads/credentials.json",
-                ["https://www.googleapis.com/auth/spreadsheets"],
-            )
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open("token.pickle", "wb") as token:
-            pickle.dump(creds, token)
+    credentials = service_account.Credentials.from_service_account_file(
+        filename=GSHEET_API_SERVICE_ACCOUNT_FILE, scopes=[GSHEET_API_SCOPES]
+    )
 
-    service = build("sheets", "v4", credentials=creds)
+    service = build("sheets", "v4", credentials=credentials)
 
     # Call the Sheets API
     return service.spreadsheets()
